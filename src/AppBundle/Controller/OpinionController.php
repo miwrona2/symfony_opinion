43d5db3e5b72771aca537c726cd684df14cbaef5
albertstol3432 <albert.stol.3432@interia.pl>
2017-12-08 01:16:27 +0100

    creating opinions works without flash alerts

--- src/AppBundle/Controller/OpinionController.php
+++ src/AppBundle/Controller/OpinionController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Opinion;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 Class OpinionController extends Controller
 {
@@ -19,6 +24,44 @@ Class OpinionController extends Controller
             ->findAll();
         
     return $this->render('opinion/index.html.twig', array('opinions' => $opinions));
+    }
+    
+    
+    /**
+     * @Route("/opinion/create", name="opinion_create")
+     */
+    public function createAction(Request $request) {
+        
+        $opinion = new Opinion;
+        
+        $form = $this->createFormBuilder($opinion)
+            ->add('content', TextType::class)
+            ->add('rate', NumberType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Opinion'))
+            ->getForm();
+        
+            $form->handleRequest($request);
+            
+            if($form->isSubmitted() && $form->isValid()) {
+                $content = $form['content']->getData();
+                $rate = $form['rate']->getData();
+                
+                $opinion->setContent($content);
+                $opinion->setRate($rate);
+                
+                $em = $this->getDoctrine()->getManager();
+                
+                $em->persist($opinion);
+                $em->flush();
+                
+//                $em->addFlash('notice',
+//                        'Opinion Added');
+                
+                return $this->redirectToRoute('opinion');
+                
+            }
+            
+            return $this->render('opinion/create.html.twig', array('form' => $form->createView()));
     }
     
 }
